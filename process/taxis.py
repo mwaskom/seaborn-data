4a44311a723f19fc521fa205b945b623f2497a33
Michael Waskom <mwaskom@gmail.com>
2021-10-09 15:12:51 -0400

    Add taxis dataset

---
+++ process/taxis.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+COLUMN_MAP = {
+    "tpep_pickup_datetime": "pickup",
+    "tpep_dropoff_datetime": "dropoff",
+    "passenger_count": "passengers",
+    "trip_distance": "distance",
+    "fare_amount": "fare",
+    "tip_amount": "tip",
+    "tolls_amount": "tolls",
+    "total_amount": "total",
+}
+
+PAYMENT_TYPES = {
+    1: "credit card",
+    2: "cash",
+}
+
+MAX_TRIP_DURATION = 8000
+
+if __name__ == "__main__":
+
+    raw = pd.read_csv(
+        "raw/taxis.csv",
+        parse_dates=["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+    ).rename(columns=str.lower)
+
+    loc = pd.read_csv("raw/taxi_zones.csv").set_index("LocationID").drop_duplicates()
+
+    clean = (
+        raw[list(COLUMN_MAP)]
+        .rename(columns=COLUMN_MAP)
+        .assign(payment=raw["payment_type"].map(PAYMENT_TYPES))
+        .assign(pickup_zone=raw["pulocationid"].map(loc["zone"]))
+        .assign(dropoff_zone=raw["dolocationid"].map(loc["zone"]))
+        .assign(pickup_borough=raw["pulocationid"].map(loc["borough"]))
+        .assign(dropoff_borough=raw["dolocationid"].map(loc["borough"]))
+        .loc[lambda x: x.eval("dropoff - pickup").dt.seconds < MAX_TRIP_DURATION]
+    )
+
+    clean.to_csv("taxis.csv", index=False)
